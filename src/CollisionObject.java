9cf693ad07803231c6a46d0257f933a81d685078
Victor Brun <victor.w.brun@gmail.com>
2021-01-23 23:11:20 +0100

    Klammerfunktioner now implemented in CollisionObject

--- src/CollisionObject.java
+++ src/CollisionObject.java
@@ -1,17 +1,50 @@
+import java.util.ArrayList;
+
 public class CollisionObject{
-    private AnalyticPath<Double, Double[]> path;
+    private ArrayList<AnalyticPath<Double, Double[]>> paths = new ArrayList<>();
+    private ArrayList<Double> upperTimeBounds = new ArrayList<>();
 
     /* Constructor */
-    public CollisionObject( AnalyticPath<Double, Double[]> path) { this.path = path; }
+    public CollisionObject( AnalyticPath<Double, Double[]> path) { this.paths.add(path); }
 
-    /* Getter for analytic path */
-    public AnalyticPath<Double, Double[]> getPath() { return this.path; }
+    /* Getter for analytic path. This returns the function with the largest upper bound */
+    public AnalyticPath<Double, Double[]> getCurrentPath() { return this.paths.get(this.paths.size()-1); }
 
-    /* Setter for analytic path */
-    public void setPath( AnalyticPath<Double, Double[]> newPath ) { this.path = newPath; }
+    /* Adder and setter for analytic path */
+    public void addPath( AnalyticPath<Double, Double[]> newPath ) { this.paths.add(newPath); }
+    public void setPath(int index, AnalyticPath<Double, Double[]> newPath) { this.paths.set(index, newPath); }
 
-    /* Evaluator for path */
-    public Double[] evalPath(Double t) {
-        return this.path.apply(t);
+    /* Evaluator for current path at given time */
+    public Double[] evalPath(double t) {
+        return this.getCurrentPath().apply( Double.valueOf(t) );
+    }
+
+    /* Evaluator for path at given index and at given time */
+    public Double[] evalPath(int index, double t) {
+        return this.paths.get(index).apply( Double.valueOf(t) );
+    }
+
+    /* Adds and gets upper bound for current, i.e. path at end of list, path */
+    public void setCurrentBound(double t) { this.upperTimeBounds.add( Double.valueOf(t) ); }
+    public double getCurrentBound() { return this.upperTimeBounds.get(this.upperTimeBounds.size() - 1).doubleValue(); }
+
+    /* Changes and gets upper time bound for function at given index */
+    public void setBound(int index, double t) { this.upperTimeBounds.set(index, Double.valueOf(t) ); }
+    public double getBound(int index) { return this.upperTimeBounds.get(index).doubleValue(); }
+
+    /* Evaluates CollisionObject position over provided interval with given resolution */
+    public double[][] calculateCurve(double t0, double T, double dt) {
+        double[][] coordMatrix = new double[2][(int)Math.ceil((T-t0)/dt) + 1];
+
+        int j = 0;
+        for (int i = 0; i < coordMatrix[0].length; i++) {
+            if (t0 > this.getBound(j)) {
+                j += 1;
+            }
+            coordMatrix[0][i] = this.evalPath(j, t0)[0].doubleValue();
+            coordMatrix[1][i] = this.evalPath(j, t0)[1].doubleValue();
+            t0 += dt;
+        }
+        return coordMatrix;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,14 @@
 public class Test {
     public static void main(String[] args) {
         // Defines pure function describing path
-        AnalyticPath<Double, Double[]> fn = new AnalyticPath<Double,Double[]>(){
-            @Override
-            public Double[] apply(Double t) {
-                return new Double[] {t*t, 10*t, t+9};
-            }
-        };
-        AnalyticBall b1 = new AnalyticBall(10, 1, 0, 1, fn);
+        AnalyticPath<Double, Double[]> fn1 = t -> new Double[] {t*t, 10*t, t+9};
+        AnalyticBall b1 = new AnalyticBall(10, 1, 0, 1, fn1);
 
 
-        Double time = Double.valueOf(9);
-        Double[] pos = b1.evalPath(time);
+        b1.setCurrentBound(20);
+        double[][] pos = b1.calculateCurve(0, 19, 1);
 
-        System.out.println("x = " + pos[0].doubleValue() );
-        System.out.println("y = " + pos[1].doubleValue() );
+        System.out.println("x = " + pos[0] );
+        System.out.println("y = " + pos[1] );
     }
 }
