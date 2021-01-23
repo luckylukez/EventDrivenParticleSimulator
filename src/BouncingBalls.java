acee36d09c74c2318619ee070d849a85f6673663
luckylukez <64895183+luckylukez@users.noreply.github.com>
2021-01-23 20:12:59 +0100

    update collision implementation

--- src/BouncingBalls.java
+++ src/BouncingBalls.java
@@ -7,65 +7,51 @@ import java.util.PriorityQueue;
 public class BouncingBalls {
     public static void main(String[] args){
 
-        ArrayList<Ball> balls = new ArrayList<>();
-        // Create balls
-        for(int i = (int) Ball.r + 5; i < World.X_PIXELS - (int) Ball.r - 5; i = i+(int)(2*Ball.r)){
-                    for(int j = (int) Ball.r + 5; j < World.Y_PIXELS - (int) Ball.r - 5; j = j+(int)(2*Ball.r)){
-                        balls.add(new Ball(i,j));
-            }
-        }
+        ArrayList<CollisionObject> balls = new ArrayList<>();
+        // TODO: create balls
 
 
 
-        // Start graphics generation on different thread
-        EventQueue.invokeLater(new Runnable() {
+        //TODO: Create collision objects for the walls
+        CollisionObject leftWall = new CollisionObject();
+        CollisionObject rightWall = new CollisionObject();
+        CollisionObject upperWall = new CollisionObject();
+        CollisionObject lowerWall = new CollisionObject();
 
-            @Override
-            public void run() {
+        ArrayList<CollisionObject> walls = new ArrayList<>();
+        walls.add(leftWall);
+        walls.add(rightWall);
+        walls.add(upperWall);
+        walls.add(lowerWall);
 
-                World world = new World(balls);
-                GridBagConstraints gbc = new GridBagConstraints();
-                gbc.gridx = 0;
-                gbc.gridy = 0;
-                gbc.weightx = 1;
-                gbc.weighty = 1;
-                gbc.fill = GridBagConstraints.BOTH;
-
-                Frame frame = new Frame();
-                frame.add(world, gbc);
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-            }
-        });
-
-        /*
         // Create collisions queue
-        //TODO: check if -1 is needed?
         PriorityQueue<Collision> collisions = new PriorityQueue<>();
         for (int i = 0; i < balls.size(); i++){ //-1 because last ball has no ball left to calculate collision with
-            Ball b1 = balls.get(i);
+            CollisionObject b1 = balls.get(i);
             for(int j = i+1; j < balls.size(); j++ ){
-                Ball b2 = balls.get(j);
-                //Collision c = b1.calculateCollision(b2);
-                //collisions.add(c);
+                CollisionObject b2 = balls.get(j);
+                Collision c = b1.calculateCollision(b2);
+                collisions.add(c);
             }
             collisions.add(b1.calculateWallCollision());
         }
-        */
+
         //####################
         //MAIN LOOP
         //####################
+        double T = 100;
+        while(collisions.peek().getCollisionTime() < T){
+            Collision nextCollision = collisions.poll();
 
-        while(true){
-            for (Ball b: balls){
-                b.move(0.5);
-            }
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if(nextCollision.isRelevant()){
+                CollisionObject[] colliders = nextCollision.getColliders();
+                double t = nextCollision.getCollisionTime();
+                for(CollisionObject obj: colliders){
+                    //TODO: calculate new path functions
+                    // save time and function data
+                }
             }
         }
     }
 }
+
--- src/Collision.java
+++ src/Collision.java
@@ -1,40 +1,32 @@
 public class Collision implements Comparable<Collision>{
-    private Ball[] balls;
+    private CollisionObject[] colliders;
     private int[] collisionsAtCreation;
-    private int amountOfBalls;
-    private int collisionType;
     public double collisionTime;
 
-    public Collision(Ball[] balls, int collisionType, double timeToCollision){
+    public Collision(CollisionObject[] colliders, double timeToCollision){
         collisionTime = System.currentTimeMillis() + timeToCollision;
-        this.balls = balls;
-        this.amountOfBalls = balls.length;
-        this.collisionType = collisionType;
-        for(int i = 0; i < amountOfBalls; i++){
-            collisionsAtCreation[i] = balls[i].getCollisionCount();
+        this.colliders = colliders;
+        for(int i = 0; i < 2; i++){
+            collisionsAtCreation[i] = colliders[i].getCollisionCount();
         }
         this.collisionTime = timeToCollision;
     }
 
-    public Ball[] balls(){
-        return balls;
+    public CollisionObject[] getColliders(){
+        return colliders;
+    }
+
+    public double getCollisionTime(){
+        return collisionTime;
     }
 
     public boolean isRelevant(){
-        boolean isRelevant = true;
-        for(int i = 0; i < amountOfBalls; i++){
-            if( balls[i].getCollisionCount() != collisionsAtCreation[i]){
-                isRelevant = false;
+        for(int i = 0; i<2; i++){
+            if(colliders[i].getCollisionCount() != collisionsAtCreation[i]){
+                return false;
             }
         }
-        return isRelevant;
-    }
-    // returns type of collision
-    // -1 for wall collisions along the x-axis
-    // 1 for wall collisions along the y-axis
-    // 0 for collisions between balls
-    public int collisionType(){
-        return collisionType;
+        return true;
     }
 
     @Override
