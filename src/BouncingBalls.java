71f3b86cdb5d1453f4d86caeaaa4511d38b84ede
luckylukez <64895183+luckylukez@users.noreply.github.com>
2021-01-24 01:03:20 +0100

    Overhaul of collision class and new template for main loop

--- src/BouncingBalls.java
+++ src/BouncingBalls.java
@@ -4,8 +4,44 @@ import java.util.ArrayList;
 import java.time.*;
 import java.util.PriorityQueue;
 
+import com.jom.DoubleMatrixND;
+import com.jom.OptimizationProblem;
+
+
 public class BouncingBalls {
     public static void main(String[] args){
+
+        int N = 5; // number of elements in each set
+
+        /* Create the optimization problem object */
+        OptimizationProblem op = new OptimizationProblem();
+
+        /* Add the decision variables to the problem */
+        op.addDecisionVariable("x", false, new int[] { N , N , N }, 0, 1);  // name, isInteger, size , minValue, maxValue
+
+        /* Set value for the input parameter c_{ijk} */
+        op.setInputParameter("c", new DoubleMatrixND(new int [] { N , N , N} , "random"));
+
+        /* Sets the objective function */
+        op.setObjectiveFunction("minimize", "sum(x .* c)");
+
+        /* Add the constraints */
+        op.addConstraint(" sum(sum(x,3),2) <= 1");  // for each i \sum_{jk} x_{ijk} <= 1
+        op.addConstraint(" sum(sum(x,3),1) <= 1");  // for each j \sum_{ik} x_{ijk} <= 1
+        op.addConstraint(" sum(sum(x,2),1) <= 1");  // for each k \sum_{ij} x_{ijk} <= 1
+
+        /* Call the solver to solve the problem */
+        op.solve("glpk" ,  "solverLibraryName" , "glpk_4_47");
+        if (!op.solutionIsOptimal ()) throw new RuntimeException ("An optimal solution was not found");
+
+        /* Print the solution */
+        DoubleMatrixND sol = op.getPrimalSolution("x");
+        for (int c1 = 0 ; c1 < N ; c1 ++)
+            for (int c2 = 0 ; c2 < N ; c2 ++)
+                for (int c3 = 0 ; c3 < N ; c3 ++)
+                    if (sol.get(new int [] { c1 , c2 , c3}) == 1)
+                        System.out.println (c1 + " - " + c2 + " - " + c3);
+
 
         ArrayList<CollisionObject> balls = new ArrayList<>();
         // TODO: create balls
@@ -42,6 +78,7 @@ public class BouncingBalls {
         double T = 100;
         while(collisions.peek().getCollisionTime() < T){
             Collision nextCollision = collisions.poll();
+
 
             if(nextCollision.isRelevant()){
                 CollisionObject[] colliders = nextCollision.getColliders();
